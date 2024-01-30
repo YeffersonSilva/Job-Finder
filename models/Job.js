ad6af0419246b0c46b3fccaf85cdaffb8563a97f
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-01-30 16:30:49 -0300

    Udate proyect

--- models/Job.js
+++ models/Job.js
@@ -1,26 +1,25 @@
-const Sequelize = require("sequelize")
-const db = require("../db/connection")
+const Sequelize = require('sequelize');
+const db = require('../db/connection');
 
-const Job = db.define('Job', {
-    titulo: {
-        type: Sequelize.STRING
-    },
-    description: {
-        type: Sequelize.STRING
-    },
-    salary: {
-        type: Sequelize.STRING
-    },
-    comapny: {
-        type: Sequelize.STRING
-    },
-    email: {
-        type: Sequelize.STRING
-    },
-  
-    new_jobs: {
-        type: Sequelize.INTEGER
-    }
-})
+const Job = db.define('job', {
+  title: {
+    type: Sequelize.STRING,
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+  salary: {
+    type: Sequelize.STRING,
+  },
+  company: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+  new_job: {
+    type: Sequelize.INTEGER,
+  }
+});
 
 module.exports = Job
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,27 +1,46 @@
 const express = require('express');
 const router  = express.Router();
 const Job     = require('../models/Job');
-///rota test
+
+// rota de teste
 router.get('/test', (req, res) => {
-    res.send('Test route');
+  res.send('deu certo');
 });
 
-//add job vis post
+// detalhe da vaga -> view/1, view/2
+router.get('/view/:id', (req, res) => Job.findOne({
+  where: {id: req.params.id}
+}).then(job => {
 
+  res.render('view', {
+    job
+  });
+
+}).catch(err => console.log(err)));
+
+
+// form da rota de envio
+router.get('/add', (req, res) => {
+  res.render('add');
+})
+
+// add job via post
 router.post('/add', (req, res) => {
-    let { titulo, salary, description, company, email, new_jobs } = req.body;
 
-    //insert
-    Job.create({
-        titulo,
-        salary,
-        description,
-        company,
-        email,
-        new_jobs
-    })
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err));
+  let {title, salary, company, description, email, new_job} = req.body;
+
+  // insert
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+  })
+  .then(() => res.redirect('/'))
+  .catch(err => console.log(err));
+
 });
 
 module.exports = router
