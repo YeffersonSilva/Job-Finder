b84377a0adeea89ae232f54f3d25cc14ca4fe299
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-01-30 16:45:23 -0300

    Haciendo conexion con Bd

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express    = require('express');
-const exphbs     = require('express-handlebars');
+const { engine } = require("express-handlebars");
 const app        = express();
 const path       = require('path');
 const db         = require('./db/connection');
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // handle bars
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // static folder
--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
-const { Sequelize } = require("sequelize")
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'db/app.db'
-})
+  dialect: 'sqlite',
+  storage: './db/app.db'
+});
 
 module.exports = sequelize
